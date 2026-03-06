498c85c414c5b16c0e2bf81bdfcb8abd231d376d
Abrar797 <ashossai@ualberta.ca>
2026-03-06 11:32:43 -0700

    delete city failed test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,14 +1,30 @@
 package com.example.lab08;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
+
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
+
         list.addCity(calgary);
-        // This will fail initially because hasCity() doesn't exist
+
         assertTrue(list.hasCity(calgary));
     }
 
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        assertTrue(list.hasCity(calgary));
+
+        list.deleteCity(calgary);
+
+        assertFalse(list.hasCity(calgary));
+    }
 }
